806636deb941cd1a38cd3ad8d933902008959028
Masashi Fujita <objectxtreme@gmail.com>
2016-05-11 21:16:07 +0900

    feat: Remove `for`

--- include/xorshift.hpp
+++ include/xorshift.hpp
@@ -1,7 +1,7 @@
 /**
  * xorshift.hpp: XorShift pseudo random number generator.
  *
- * Copyright (c) 2016 Polyphony Digital Inc.
+ * Copyright (c) 2016 Masashi Fujita
  */
 #pragma once
 #ifndef xorshift_hpp__b71b3a16_63c6_402e_881e_d6327a69180f
@@ -58,7 +58,7 @@ namespace XorShift {
         __asm__ ("leaq %1, %%rsi\n"
                  "movq (%%rsi), %%rax  \n"
                  "movq 8(%%rsi), %%rdx \n"
-                 "xorshift_retry_EFF4AF5C_2F3E_4D8F_9DAE_9D1CFD6444B9_%=:"
+                 "xorshift_retry_EFF4AF5C_2F3E_4D8F_9DAE_9D1CFD6444B9_%=:   \n"
                  "movq %%rdx, %%rbx \n"
                  "movq %%rax, %%rcx \n"
                  "movq %%rax, %%rsi \n"
@@ -96,22 +96,22 @@ namespace XorShift {
     }
 
     inline state_t &    jump (state_t &state) {
-        static const std::array<uint64_t, 2> JUMP { 0x8a5cd789635d2dff, 0x121fd2155c472f96 } ;
-
         uint_fast64_t s0 = 0 ;
         uint_fast64_t s1 = 0 ;
 
-        for (auto jmp : JUMP) {
+        auto update = [&s0, &s1](state_t &S, uint64_t mask) {
             for (int_fast32_t b = 0 ; b < 64 ; ++b) {
-                uint64_t v0 = state [0] ;
-                uint64_t v1 = state [1] ;
-                if ((jmp & (1ull << b)) != 0) {
+                auto v0 = S [0] ;
+                auto v1 = S [1] ;
+                if ((mask & (1ull << b)) != 0) {
                     s0 ^= v0 ;
                     s1 ^= v1 ;
                 }
-                next (state) ;
+                next (S) ;
             }
-        }
+        } ;
+        update (state, 0x8a5cd789635d2dffull) ;
+        update (state, 0x121fd2155c472f96ull) ;
         state [0] = s0 ;
         state [1] = s1 ;
         return state ;
